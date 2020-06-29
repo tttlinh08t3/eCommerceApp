dc720b968bcde65b385121297f61184fbf64ff90
Tran Thi Thuy Linh <tttlinh08t3@users.noreply.github.com>
2020-06-29 23:43:33 +1000

    Update product service

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -12,23 +12,11 @@ export class ProductService {
     private products: Product[] = [];
     constructor(private http: HttpClient, private authService: AuthService) {}
 
-//   storeRecipes() {
-//     const recipes = this.recipeService.getRecipes();
-//     this.http
-//       .put(
-//         'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json',
-//         recipes
-//       )
-//       .subscribe(response => {
-//         console.log(response);
-//       });
-//   }
-
   fetchProducts() {
     this.authService.user.pipe(take(1), exhaustMap(user => {
         return this.http
         .get<Product[]>(
-            'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json', // will change
+            '', // will change
             {
                 params: new HttpParams().set('auth', user.getToken())
             }
